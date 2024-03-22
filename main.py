88054c439cff9021755bbf6ad7006c2ed086e654
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-03-22 15:42:12 -0300

    Add openai and langchain-openai packages, and load environment variables in main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from langchain.chains.llm import LLMChain
-from langchain_community.chat_models.openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 
 if __name__ == "__main__":
+    load_dotenv()
+
     summary_template = """
     Given the information {information} about a person I want to create:
     1. A short summary
@@ -21,9 +25,12 @@ if __name__ == "__main__":
     )
 
     llm = ChatOpenAI(
-        temperature=0,
-        model_name="gpt-3.5-turbo",
+        organization = os.getenv('OPENAI_ORG'),
+        temperature = 0,
+        model_name = "gpt-3.5-turbo",
     )
 
     chain = LLMChain(llm = llm, prompt = summary_prompt)
     res = chain.invoke(input = {"information": information})
+
+    print(res)
